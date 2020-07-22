0285f5389b3d3e8899bb816ee50847368e78334b
Bartek <bartek.stawiarski16@gmail.com>
2020-07-23 00:39:42 +0200

    add classes responsible for adding magazines

--- src/pl/library/io/DataReader.java
+++ src/pl/library/io/DataReader.java
@@ -1,6 +1,7 @@
 package pl.library.io;
 
 import pl.library.model.Book;
+import pl.library.model.Magazine;
 
 import java.util.Scanner;
 
@@ -32,5 +33,21 @@ public class DataReader {
         int pages = getInt();
 
         return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+    public Magazine readAndCreateMagazine() {
+        System.out.println("Tytuł: ");
+        String title = sc.nextLine();
+        System.out.println("Wydawnictwo: ");
+        String publisher = sc.nextLine();
+        System.out.println("Język: ");
+        String language = sc.nextLine();
+        System.out.println("Rok wydania: ");
+        int year = getInt();
+        System.out.println("Miesiąc: ");
+        int month = getInt();
+        System.out.println("Dzień: ");
+        int day = getInt();
+
+        return new Magazine(title,publisher,day,month,year,language);
     }
 }
--- src/pl/library/model/Library.java
+++ src/pl/library/model/Library.java
@@ -2,12 +2,15 @@ package pl.library.model;
 
 public class Library {
 
-    private final int maxBooks = 1000;
-    private Book[] books = new Book[maxBooks];
+    private final static int MAX_BOOKS = 1000;
+    private final static int MAX_MAGAZINES = 1000;
+    private Book[] books = new Book[MAX_BOOKS];
+    private Magazine[] magazines = new Magazine[MAX_MAGAZINES];
     private int booksNumber = 0;
+    private int magazinesNumber = 0;
 
     public void addBook(Book book){
-        if(booksNumber < maxBooks){
+        if(booksNumber < MAX_BOOKS){
             books[booksNumber] = book;
             booksNumber++;
         }else{
@@ -21,6 +24,24 @@ public class Library {
         }
         for (int i = 0; i < booksNumber; i++) {
             books[i].printInfo();
+        }
+    }
+
+    public void addMagazine(Magazine magazine){
+        if(magazinesNumber < MAX_MAGAZINES){
+            magazines[magazinesNumber] = magazine;
+            magazinesNumber++;
+        }else{
+            System.out.println("Osiągnięto maksymalną liczbę magazynów w bibliotece!");
+        }
+    }
+
+    public void printMagazines(){
+        if(magazinesNumber == 0){
+            System.out.println("Brak magazynów w bibliotece");
+        }
+        for(int i = 0 ; i < magazinesNumber ; i++){
+            magazines[i].printInfo();
         }
     }
 
